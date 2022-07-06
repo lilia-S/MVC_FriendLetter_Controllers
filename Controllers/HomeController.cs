8881869cbe52a0ad9b390bef7b1c242f913ace30
liliia87 <lilia.krivelyova@gmail.com>
2022-07-05 19:44:43 -0700

    initialcommit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendLetter.Controllers
+{
+    [Route("/hello")]
+    public string Hello() { return "Hello friend!"; }
+
+    [Route("/goodbye")]
+    public string Goodbye() { return "Goodbye friend."; }
+
+    [Route("/")]
+    public string Letter() { return "Our virtual postcard will go here soon!"; }
+  }
+}
